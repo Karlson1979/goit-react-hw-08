87abfe37783b5a27af6c7f8fbc4b85c70189a3d5
Tochka <tochka114@gmail.com>
2024-11-25 13:55:19 +0200

    title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import css from "./HomePage.module.css";
 import { GrStatusGood } from "react-icons/gr";
 
 const HomePage = () => {
+  useEffect(() => {
+    document.title = "Phone Book";
+  }, []);
   return (
     <>
       <h1 className={css.title}>PHONE BOOK</h1>
